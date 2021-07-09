e1c91ca726332f69656e06b4df8522263bf55137
Mario Kletečki <mario.kletecki@fer.hr>
2021-07-09 18:12:59 +0200

    add catkin workspace

---
+++ catkin_franka_emika_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/mario/catkin_franka_emika_ws/devel:/opt/ros/melodic'
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
+export PWD='/home/mario/catkin_franka_emika_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/mario/catkin_franka_emika_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/mario/catkin_franka_emika_ws/src:/opt/ros/melodic/share'
