9ffd2f20e30f1401c41c4de81dbbf06c98fd9ab3
Mario Kletečki <mario.kletecki@fer.hr>
2021-07-09 19:02:41 +0200

    add franka gazebo moveit

--- catkin_franka_emika_ws/build/franka_gazebo_moveit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mario/catkin_franka_emika_ws/src/franka_gazebo_moveit
-# Build directory: /home/mario/catkin_franka_emika_ws/build/franka_gazebo_moveit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
