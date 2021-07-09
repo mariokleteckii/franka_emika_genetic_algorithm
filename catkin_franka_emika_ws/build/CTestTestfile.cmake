a26a12b47a61e0043735578a0e9f9042f99abac3
Mario Kletečki <mario.kletecki@fer.hr>
2021-07-09 18:16:10 +0200

    delete catkin workspace

--- catkin_franka_emika_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mario/catkin_franka_emika_ws/src
-# Build directory: /home/mario/catkin_franka_emika_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("franka_gazebo_moveit")
